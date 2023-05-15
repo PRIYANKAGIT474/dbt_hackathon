8c609deebb15df7975165fcdac25987217d70d74
rnethala@landmarkhealth.org <rnethala@landmarkhealth.org>
2023-05-15 17:36:45 +0000

    commit

---
+++ models/example/vw_User_EncounterDetails.sql
@@ -0,0 +1,30 @@
+with Users as (
+
+    select * from {{ ref('vw_UserDetails') }}
+
+),
+
+Encounters as (
+
+    select * from {{ ref('vw_EncounterStatus') }}
+
+),
+ 
+
+final as (
+
+    select
+        E.HealthPlan_City,
+        E.Provider_login,
+        U.User_FullName as Provider_name,
+        E.Encounter_Doc_Status,
+        E.Encounter_date,
+        Count(distinct E.Encounter_Id) as visits
+
+    from Encounters E
+     join Users U ON  E.Provider_login = U.User_login_name
+     Group by 1,2,3,4,5
+
+)
+
+select * from final
