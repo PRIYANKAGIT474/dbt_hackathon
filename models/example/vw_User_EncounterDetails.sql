302bafba5c282ee8dbc8698d5eedf8afe17f79df
rnethala@landmarkhealth.org <rnethala@landmarkhealth.org>
2023-05-16 13:58:16 +0000

    commit

--- models/example/vw_User_EncounterDetails.sql
+++ models/example/vw_User_EncounterDetails.sql
@@ -18,8 +18,9 @@ final as (
         E.Provider_login,
         U.User_FullName as Provider_name,
         E.Encounter_Doc_Status as Encounter_Status,
-        DateName( month , DateAdd( month ,month(E.Encounter_date), -1 ) ),
+        --DateName( month , DateAdd( month ,month(E.Encounter_date), -1 ) ),
        --datename(month,E.Encounter_date) as Month_of_Encounter,
+       Month_name(E.Encounter_date) as Month_of_Encounter,
        --convert(char(3), [E.Encounter_date], 0),
         Count(distinct E.Encounter_Id) as No_of_Encounters
 
